202a1261b02ef794c6af13e78aeb2ab44422c922
kumaru <akmal0711@gmail.com>
2017-11-09 11:08:10 +0800

    New Update

--- app/src/main/java/com/example/alarmnotification/AlarmNotificationService.java
+++ app/src/main/java/com/example/alarmnotification/AlarmNotificationService.java
@@ -37,7 +37,7 @@ public class AlarmNotificationService extends IntentService {
 
         //Create notification
         NotificationCompat.Builder alamNotificationBuilder = new NotificationCompat.Builder(
-                this).setContentTitle("Alarm").setSmallIcon(R.mipmap.ic_launcher)
+                this).setContentTitle("Alarm").setSmallIcon(R.mipmap.gundam)
                 .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
                 .setContentText(msg).setAutoCancel(true);
         alamNotificationBuilder.setContentIntent(contentIntent);
